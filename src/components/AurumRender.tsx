020a59a821b36bb98e2a7376f02a3f4f71eee299
MIGUEL ANGEL DELGADO VERGARA <mgld_09@hotmail.com>
2026-09-14 21:29:36 -0500

    Completar imports de React para AURUM RENDER público

--- src/components/AurumRender.tsx
+++ src/components/AurumRender.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { applyAurumMetal, applyAurumGem, metalPresetFromConfig, gemPresetFromConfig } from "../lib/aurum-material-engine";
 import { getAurumGemPreset, createAurumInclusionConfig, generateAurumInclusionPoints, getAurumOpticalProfile, applyAurumOpticalProfile, applyAurumDiamondOptics } from "../lib/aurum-material-engine";
