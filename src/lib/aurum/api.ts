35df35f0c505f0303e2b40c29c66bcb8dfb36f04
MIGUEL ANGEL DELGADO VERGARA <mgld_09@hotmail.com>
2026-09-15 23:42:02 -0500

    refactor: centralize Aurum viewer API facade

---
+++ src/lib/aurum/api.ts
@@ -0,0 +1,19 @@
+export function createAurumApi(options:any){
+  return {
+    cargar: options.cargar,
+    material: options.material,
+    gema: options.gema,
+    escenario: options.escenario,
+    hdriGround: options.hdriGround,
+    iluminacion: options.iluminacion,
+    sceneStudio: options.sceneStudio,
+    reset: options.reset,
+    autoRotar: options.autoRotar,
+    capturar: options.capturar,
+    limpiar: options.limpiar,
+    partes: options.partes,
+    seleccionarParte: options.seleccionarParte,
+    fullscreen: options.fullscreen,
+    vista: options.vista,
+  };
+}
