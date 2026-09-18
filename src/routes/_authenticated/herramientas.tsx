852464f79b5186c7d7cd6f83f5077478adf7c047
MIGUEL ANGEL DELGADO VERGARA <mgld_09@hotmail.com>
2026-09-18 12:47:37 -0500

    Desactivar temporalmente programador de horno

--- src/routes/_authenticated/herramientas.tsx
+++ src/routes/_authenticated/herramientas.tsx
@@ -5,7 +5,6 @@ import { MobileBackButton } from "@/components/AppShell";
 import { Gem } from "lucide-react";
 import { VisorPesoJoyeria } from "@/components/VisorPesoJoyeria";
 import { CalculadoraYeso } from "@/routes/_authenticated/taller";
-import { ProgramadorHornoCasting } from "@/components/ProgramadorHornoCasting";
 
 export const Route = createFileRoute("/_authenticated/herramientas")({
   head: () => ({
@@ -84,7 +83,6 @@ function HerramientasPage() {
             <CalculadoraAleacionOro />
             <VisorPesoJoyeria />
             <ConversorTallasAnillo />
-            <ProgramadorHornoCasting />
           </div>
         </section>
 
