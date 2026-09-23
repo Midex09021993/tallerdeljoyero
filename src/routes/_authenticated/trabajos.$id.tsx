02e9e950ba5730dc97d89c2706ed6d977eeef492
MIGUEL ANGEL DELGADO VERGARA <mgld_09@hotmail.com>
2026-09-23 08:32:46 -0500

    fix: importar Link en ficha tecnica del operario

--- src/routes/_authenticated/trabajos.$id.tsx
+++ src/routes/_authenticated/trabajos.$id.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { ArrowLeft, Check, CircleAlert, ChevronDown, FileText, Link2, Play, Paperclip } from "lucide-react";
 import { AppShell } from "@/components/AppShell";
